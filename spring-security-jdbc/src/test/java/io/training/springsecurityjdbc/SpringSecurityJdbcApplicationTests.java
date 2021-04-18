d40c4a1c519bf5723131494925aeb6e584ec586f
Ardelean <ardelev@local>
2021-04-18 21:55:10 +0300

    complete spring security - jpa integration

---
+++ spring-security-jdbc/src/test/java/io/training/springsecurityjdbc/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.training.springsecurityjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-security-jpa/src/main/java/io/training/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,16 @@
+package io.training.springsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
